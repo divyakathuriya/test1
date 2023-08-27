33cee24aa34811c6fb36e574c2175510a0fa317f
deepakkathuria <deepakkathuria32@gmail.com>
2023-08-27 10:33:25 +0530

    front ecom

--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -7,7 +7,8 @@ interface BillboardProps {
 const Billboard: React.FC<BillboardProps> = ({
   data
 }) => {
-  console
+  console.log(data,"datataa")
+  
   return ( 
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
       <div style={{ backgroundImage: `url(${data?.imageUrl})` }} className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
